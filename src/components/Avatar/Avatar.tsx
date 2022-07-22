dddb38d840dae06921212d7a25216f42314f24b9
Timóteo Stifft <timoteostifft@gmail.com>
2022-07-21 23:41:15 -0300

    refactor: update avatar component properties

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
+import React, { ImgHTMLAttributes } from 'react';
 
 import styles from './Avatar.module.css'
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
   hasBorder?: boolean;
-  src: string;
-  alt?: string
 }
 
-function Avatar({ hasBorder = true, src, alt }: AvatarProps) {
+function Avatar({ hasBorder = true, ...props }: AvatarProps) {
   return (
     <img
       className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-      src={src}
-      alt={alt}
+      {...props}
     />
   );
 }
